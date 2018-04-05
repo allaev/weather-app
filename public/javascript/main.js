c74ff45306beb608797f809b590824b51e32d3a4
allaev <34710484+allaev@users.noreply.github.com>
2018-04-06 01:22:29 +0500

    add background images

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,9 +1,25 @@
 /* global $ */
 
 var temp=$("#temperature").html();
-  if (temp>"50") {
-    $("body").css("background-color", "red");    
+  if (temp<="0") {
+    $("body").css("background-image", "url(../images/0.jpg)");    
   }
-  else {    
-    $("body").css("background-color", "blue");    
+  else if (temp<="16") {
+    $("body").css("background-image", "url(../images/16.jpg)");    
   }
+  else if (temp<"32") {
+    $("body").css("background-image", "url(../images/32.jpg)");    
+  }
+  else if (temp<"50") {
+    $("body").css("background-image", "url(../images/50.jpg)");    
+  }
+  else if (temp<"59") {
+    $("body").css("background-image", "url(../images/59.jpg)");    
+  }
+  else if (temp<"68") {
+    $("body").css("background-image", "url(../images/68.jpg)");    
+  }
+  else {
+    $("body").css("background-image", "url(../images/70.jpg)");    
+  }
+  
